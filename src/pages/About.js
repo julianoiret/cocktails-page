9a272338b1274a658a8b65fe4b8d631aac14a610
Julia Noiret <julia.noiret@gmail.com>
2021-09-30 13:26:21 +1000

    project completed

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, ducimus vel rem eaque, ipsam numquam consequuntur minus eligendi voluptate doloremque accusantium excepturi debitis doloribus quasi quam voluptatum ad dolores soluta!</p>
+    </section>
+  )
+}
+
+export default About
